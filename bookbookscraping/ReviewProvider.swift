9d95886c656ce4d860cedfa1a96d044aab541653
minomi <5minhominho@gmail.com>
2018-05-18 04:04:40 +0900

    gitignore 추가

---
+++ bookbookscraping/ReviewProvider.swift
@@ -0,0 +1,9 @@
+//
+//  ReviewProvider.swift
+//  bookbookscraping
+//
+//  Created by 오민호 on 2018. 5. 18..
+//  Copyright © 2018년 오민호. All rights reserved.
+//
+
+import Foundation
---
+++ bookbookscraping/ServerAPI.swift
@@ -0,0 +1,9 @@
+//
+//  ServerAPI.swift
+//  bookbookscraping
+//
+//  Created by 오민호 on 2018. 5. 17..
+//  Copyright © 2018년 오민호. All rights reserved.
+//
+
+import Foundation
